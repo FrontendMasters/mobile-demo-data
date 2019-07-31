653bd139e2aa62d44f57c271243919a778d4ccb9
Blagovest Dachev <blago@dachev.com>
2019-07-30 21:50:26 -0500

    Renamed a property

--- extract.js
+++ extract.js
@@ -3,11 +3,11 @@
 var fs = require('fs');
 
 let text = fs.readFileSync('./deleteme.json', 'utf8');
-let films = JSON.parse(text);
+let movies = JSON.parse(text);
 
-for (key in films) {
-  let film = films[key];
-  let text = JSON.stringify(film, null, 2);
+for (key in movies) {
+  let movie = movies[key];
+  let text = JSON.stringify(movie, null, 2);
 
-  fs.writeFileSync(`./films/${film.id}.json`, text);
+  fs.writeFileSync(`./movies/${movie.id}.json`, text);
 }
