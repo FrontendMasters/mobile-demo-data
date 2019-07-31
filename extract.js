74173465724bd7e83c74b880b5e535c418e7d1e1
Blagovest Dachev <blago@dachev.com>
2019-07-31 12:07:04 -0500

    Fixed broken image URLs

--- extract.js
+++ extract.js
@@ -2,12 +2,12 @@
 
 var fs = require('fs');
 
-let text = fs.readFileSync('./deleteme.json', 'utf8');
+let text = fs.readFileSync('./data/movies.json', 'utf8');
 let movies = JSON.parse(text);
 
 for (key in movies) {
   let movie = movies[key];
   let text = JSON.stringify(movie, null, 2);
 
-  fs.writeFileSync(`./movies/${movie.id}.json`, text);
+  fs.writeFileSync(`./data/movies/${movie.id}.json`, text);
 }
